0e1b0359cc2761b7ce281b9eaffa5c8c17253557
CuttlefishRCool <spincerbrock@gmail.com>
2026-01-23 18:55:36 -0600

    final

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 function getChange(e) {
 var give = document.getElementById('amount-received').value;
 var take = document.getElementById('amount-due').value;
-
+document.getElementById("amount-remaining").textContent = '';
 
 //initialize temp variables
 var quarters = 0;
@@ -31,10 +31,14 @@ while(result > 0){
     } else if (result.toFixed(2) >= .05){
         nickels++;
         result-= .05;
-    } else if (result.toFixed(2) >= .01){
+    } else if ((Math.floor(result * 100) / 100) >= .01){
         pennies++;
         result-= .01;
-    } else { break; }
+    } else { 
+        if(result > 0.00){
+        document.getElementById("amount-remaining").textContent = "You'll lose " + result + "!";
+        } 
+        break; }
 }   
     document.getElementById("dollars-output").textContent = dollars;
     document.getElementById("quarters-output").textContent = quarters;
@@ -42,5 +46,24 @@ while(result > 0){
     document.getElementById("nickels-output").textContent = nickels;
     document.getElementById("pennies-output").textContent = pennies;
 }
+ $.getJSON('currencies.json', function(currencies) {
+    const sel = document.getElementById('currencies');
+    currencies.forEach(currenc => {
+        var option = document.createElement("option");
+        option.value = currenc.exRate;
+        option.textContent = currenc.Currency;
+
+        sel.appendChild(option);
+    });
+ });
+function getExchange(e){
+let selectedC = document.getElementById('currencies').value;
+let amountH = document.getElementById('amount-have').value;
+let dolla = amountH * selectedC;
+document.getElementById('amount-received').value = dolla;
+document.getElementById('amount-due').value = 0;
+getChange();
+}
 // write a button that initiates getChange function
 document.getElementById("calculate-change").onclick = getChange;
+document.getElementById("calculate-rate").onclick = getExchange;
